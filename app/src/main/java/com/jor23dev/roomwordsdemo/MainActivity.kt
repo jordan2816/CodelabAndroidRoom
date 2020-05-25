0f033b2ff936255499c9dc0eb2b13b292e401e85
Jordan <jordan_lee@cathaypacific.com>
2020-05-25 17:15:05 +0800

    first commit

---
+++ app/src/main/java/com/jor23dev/roomwordsdemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jor23dev.roomwordsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.layoutManager = LinearLayoutManager( this)
+    }
+}
